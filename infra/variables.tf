6307ee3566848532fb3fa3d1c3a33d46f3875704
root <root@DESKTOP-0V4NUDQ.localdomain>
2025-10-14 17:21:19 -0300

    fix: variables04.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,6 +1,6 @@
 variable "app_name" {
   type    = string
-  default = "ms-saudacoesaleatorias"
+  default = "mssaudacoesaleatorias"
 }
 
 variable "service_name" {
@@ -20,7 +20,7 @@ variable "container_port" {
 
 variable "docker_image_name" {
   type    = string
-  default = "nilsonmazurchi/ms-saudacoes-aleatorias"
+  default = "xxxxxx/ms-saudacoes-aleatorias"
 }
 
 variable "docker_image_tag" {
